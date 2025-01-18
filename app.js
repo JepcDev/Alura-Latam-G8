6eca731b64c3827c1e50ffeaa1380daeaecb0984
JepcDev <joseenrique.pc21@gmail.com>
2025-01-18 00:13:03 -0400

    feat: add promp for number user

---
+++ app.js
@@ -0,0 +1,11 @@
+// let numeroSecreto = 6;
+// let numeroUsuario = prompt("Me indicas un numero por favor:");
+
+// console.log(numeroUsuario);
+
+// if (numeroUsuario == numeroSecreto) {
+//     alert('Acertaste el numero');
+// }
+
+alert("Hello world");
+let numeroUsuario = prompt("Me indicas un número por favor: ");
