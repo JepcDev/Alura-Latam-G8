ec53c26273249d17c4e826c1d0fe167f16880bda
JepcDev <joseenrique.pc21@gmail.com>
2025-01-19 00:25:19 -0400

    feat: Complete section 1 getting started with js

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-// let numeroSecreto = 6;
-// let numeroUsuario = prompt("Me indicas un numero por favor:");
-
-// console.log(numeroUsuario);
-
-// if (numeroUsuario == numeroSecreto) {
-//     alert('Acertaste el numero');
-// }
-
-alert("Hello world");
+// alert("Hello world");
+let numeroSecreto = 5;
 let numeroUsuario = prompt("Me indicas un número por favor: ");
+let nombreUsuario = "Lua"
+let edadUsuario = 25;
+let numeroVentas = 50;
+let saldoDisponible = 1000;
+let mensajeError = '¡Error! Completa todos los campos';
+
+alert('¡Bienvenido a nuestro sitio web');
+alert('¡Error! Completa todos los campos');
+alert("variable mensajeError" + mensajeError);
+
+console.log(numeroUsuario);
+nombreUsuario = prompt('Digite su nombre:');
+edadUsuario = prompt('Digite su edad: ');
+
+if (numeroUsuario == numeroSecreto) {
+  alert('Felicidades!! Acestaste el número');
+}
+
+if (edadUsuario>= 18) {
+  alert('¡Puedes obtener tu licencia de conducir!');
+}
