83f19543ad0305aee7bddc091fb2ef1e176e770d
JepcDev <joseenrique.pc21@gmail.com>
2025-01-19 23:51:48 -0400

    feat: add template strings

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ let numeroVentas = 50;
 let saldoDisponible = 1000;
 let mensajeError = '¡Error! Completa todos los campos';
 
-alert('¡Bienvenido a nuestro sitio web');
-alert('¡Error! Completa todos los campos');
-alert("variable mensajeError" + mensajeError);
+// alert('¡Bienvenido a nuestro sitio web');
+// alert('¡Error! Completa todos los campos');
+// alert("variable mensajeError" + mensajeError);
 
-console.log(numeroUsuario);
-nombreUsuario = prompt('Digite su nombre:');
-edadUsuario = prompt('Digite su edad: ');
+// console.log(numeroUsuario);
+// nombreUsuario = prompt('Digite su nombre:');
+// edadUsuario = prompt('Digite su edad: ');
 
 if (numeroUsuario == numeroSecreto) {
-  alert('Felicidades!! Acestaste el número');
+  alert(`Felicidades!! Acestaste, el número es: ${numeroSecreto}`);
 }else{
   // No se cumplio la condicion
   alert('Lo siento, no acertaste el número!');
 }
 
-if (edadUsuario>= 18) {
-  alert('¡Puedes obtener tu licencia de conducir!');
-}
+// if (edadUsuario>= 18) {
+//   alert('¡Puedes obtener tu licencia de conducir!');
+// }
