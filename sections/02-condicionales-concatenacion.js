8e8d71987ba359a49c75981f806d55a5520bb5c6
JepcDev <joseenrique.pc21@gmail.com>
2025-01-21 22:02:06 -0400

    feat: add desafio section 2

---
+++ sections/02-condicionales-concatenacion.js
@@ -0,0 +1,54 @@
+// alert("Hello world");
+let numeroSecreto = 5;
+let numeroUsuario = prompt("Me indicas un número entre el 1 y 10 por favor: ");
+let nombreUsuario = "Lua"
+let edadUsuario = 25;
+let numeroVentas = 50;
+let saldoDisponible = 1000;
+let mensajeError = '¡Error! Completa todos los campos';
+
+let diaSemana = prompt("Que día de la semana es?:");
+
+alert('¡Bienvenido a nuestro sitio web'+nombreUsuario);
+// alert('¡Error! Completa todos los campos');
+// alert("variable mensajeError" + mensajeError);
+
+// TODO -> Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
+// let mensajeSaldo =
+
+// console.log(numeroUsuario);
+// nombreUsuario = prompt('Digite su nombre:');
+// edadUsuario = prompt('Digite su edad: ');
+
+if (numeroUsuario>=1) {
+  alert('Su numero es positivo!');
+}else{
+  if (numeroUsuario<0) {
+    alert('Su numero es negativo!');
+  }else{
+    alert('su numero no es numero');
+  }
+}
+
+if (puntuacion>=100) {
+  alert('Felicidades, has ganado!')
+}else{
+  alert('intentalo nuevamente para ganar!')
+}
+
+if (diaSemana == 'Sabado' || diaSemana==Domingo) {
+  alert('¡Buen din de semana!');
+}else{
+  alert('¡Buena semana!')
+}
+
+if (numeroUsuario == numeroSecreto) {
+  alert(`Felicidades!! Acestaste, el número es: ${numeroSecreto}`);
+}else{
+  // No se cumplio la condicion
+  alert('Lo siento, no acertaste el número!');
+}
+
+// if (edadUsuario>= 18) {
+//   alert('¡Puedes obtener tu licencia de conducir!');
+// }
