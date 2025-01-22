fd183eeacfbee8dd00ae4c11650458e1b32bbf96
JepcDev <joseenrique.pc21@gmail.com>
2025-01-21 23:03:37 -0400

    feat: add loops y bucles, add bucle while para volver a preguntar por un numero

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 
 let numeroSecreto = 5;
-let numeroUsuario = prompt("Me indicas un número entre el 1 y 10 por favor: ");
+let numeroUsuario = 0;
 
-console.log(numeroUsuario);
+while( numeroSecreto !== numeroUsuario){
+  numeroUsuario = prompt("Me indicas un número entre el 1 y 10 por favor: ");
 
-if (numeroUsuario == numeroSecreto) {
-  alert(`Felicidades!! Acestaste, el número es: ${numeroSecreto}`);
-}else{
-  if (numeroSecreto > numeroUsuario) {
-    alert('El número secreto es menor');
+  console.log(numeroUsuario);
+
+  if (numeroUsuario == numeroSecreto) {
+    alert(`Felicidades!! Acestaste, el número es: ${numeroSecreto}`);
   }else{
-    alert('El número secreto es mayor');
+    if (numeroSecreto > numeroUsuario) {
+      alert('El número secreto es menor');
+    }else{
+      alert('El número secreto es mayor');
+    }
+    // No se cumplio la condicion
+    // alert('Lo siento, no acertaste el número!');
   }
-  // No se cumplio la condicion
-  // alert('Lo siento, no acertaste el número!');
 }
