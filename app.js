46c426f8f94b56db42c3356bd424db67f7684996
JepcDev <joseenrique.pc21@gmail.com>
2025-01-21 23:26:02 -0400

    feat: add contador de intentos

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 
 let numeroSecreto = 5;
 let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
 
-while( numeroSecreto !== numeroUsuario){
+while( numeroUsuario!= numeroSecreto){
   numeroUsuario = prompt("Me indicas un número entre el 1 y 10 por favor: ");
 
-  console.log(numeroUsuario);
+  // console.log(numeroUsuario);
 
   if (numeroUsuario == numeroSecreto) {
-    alert(`Felicidades!! Acestaste, el número es: ${numeroSecreto}`);
+    alert(`Felicidades!! Acestaste, el número es: ${numeroSecreto}. Lo hiciste en ${intentos} veces`);
   }else{
-    if (numeroSecreto > numeroUsuario) {
+    if (numeroUsuario > numeroSecreto) {
       alert('El número secreto es menor');
     }else{
       alert('El número secreto es mayor');
     }
+    intentos++;
+    palabraVeces = 'veces';
     // No se cumplio la condicion
     // alert('Lo siento, no acertaste el número!');
   }
