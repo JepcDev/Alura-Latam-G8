5edc1bbc306aa52eb0e946fb1f6ff94471ec3806
JepcDev <joseenrique.pc21@gmail.com>
2025-01-22 23:13:39 -0400

    feat: add chanllege and section loops, tentativas

---
+++ sections/chanlleges-section-03.js
@@ -0,0 +1,33 @@
+
+
+// DEV:TODO -> task: Crear un contador que comience en 1 y vaya hasta el 10 usando un bucle 'while'. Mostrar cada numero.
+// let  counter = 1;
+
+// while (counter<=10) {
+//   console.log('counter = ' + counter);
+//   counter++;
+// }
+
+// DEV:TODO -> task: Crear un contador que comience ne 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+// let count = 10;
+
+// while (count>=0) {
+//   console.log('Counter = '+ count);
+//   count--;
+// }
+
+// DEV:TODO -> task: Crea un programa de cuenta regresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+// let countdown = prompt('Ingrese un numero para la cuenta regresiva');
+
+// while (countdown>=0) {
+//   console.log('CountDown = '+ countdown);
+//   countdown--;
+// }
+
+// DEV:TODO -> Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+// let countUser = prompt('Ingrese un numero para la cuenta progresiva');
+// let count = 0;
+// while (count<=countUser) {
+//   console.log('Counter = '+ count );
+//   count++;
+// }
