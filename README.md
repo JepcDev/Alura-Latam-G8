c6c385b41df60fd2e86fd1c107e08b2ea2b494ae
JepcDev <joseenrique.pc21@gmail.com>
2025-02-02 23:08:41 -0400

    feat: add first commit, elements DOM

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# Alura-Latam-G8" 
+"# Alura-Latam-G8: stage-1/beginner-programmer/course-2-programming-logic-functions-lists"
--- app.js
+++ app.js
@@ -1,37 +1,7 @@
 
-// let numeroSecreto = 5;
-let numeroMaximoPosible = 10;
-let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
-let numeroUsuario = 0;
-let intentos = 1;
-// let palabraVeces = 'vez';
-let maximoIntnetos = 3;
 
-while( numeroUsuario!= numeroSecreto){
-  numeroUsuario = parseInt(prompt(`Me indicas un número entre el 1 y ${numeroMaximoPosible} por favor: `));
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto';
 
-  // console.log(numeroUsuario);
-
-  if (numeroUsuario == numeroSecreto) {
-    alert(`Felicidades!! Acestaste, el número es: ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos ==1 ? 'vez' : 'veces'}`);
-  }else{
-    if (numeroUsuario > numeroSecreto) {
-      alert('El número secreto es menor');
-    }else{
-      alert('El número secreto es mayor');
-    }
-
-    // incrementamos el contador cuando no acierta
-    // intentos = intentos +1;
-    // intentos +=1;
-    intentos++;
-
-    // palabraVeces = 'veces';
-    if (intentos>maximoIntnetos) {
-      alert(`Llegaste al número maximo de ${maximoIntnetos} intentos`);
-      break;
-    }
-    // No se cumplio la condicion
-    // alert('Lo siento, no acertaste el número!');
-  }
-}
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un número del 1 al 10';
