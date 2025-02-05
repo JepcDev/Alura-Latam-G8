a2153b2698ba75734b452ea4b27d2c3845b020b8
JepcDev <joseenrique.pc21@gmail.com>
2025-02-04 22:29:52 -0400

    feat: asignar texto a un elemento HTML

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 
 
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Juego del número secreto';
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10';
+// let parrafo = document.querySelector('p');
+// parrafo.innerHTML = 'Indica un número del 1 al 10';
+
+function asignarTextoElemento(elemento, texto) {
+  // let titulo = document.querySelector('h1');
+  let elementoHTML = document.querySelector(elemento);
+  // titulo.innerHTML = 'Juego del número secreto';
+  elementoHTML.innerHTML = texto;
+}
 
 function intentoUsuario(){
   alert('Click desde el botón');
 }
+
+asignarTextoElemento('h1','Juego del número secreto');
+asignarTextoElemento('p','Indica un número del 1 al 100');
