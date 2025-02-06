3b7abb0a520e8c83ad489ee16d4b1de2881321d1
JepcDev <joseenrique.pc21@gmail.com>
2025-02-05 22:10:39 -0400

    feat: add section02 functions

---
+++ sections/challege_section02.js
@@ -0,0 +1,24 @@
+
+// DEV:TODO -> 1. Crear una función que muestre "¡Hola, mundo!" en la consola.
+// function saludo() {
+//   console.log('Hola Mundo');
+// }
+
+// saludo();
+
+// DEV:TODO -> 2. Crear una función que reciba un nombre como parámetro y muestre "¡Hola, [nombre]!" en la consola.
+// function saludo2(nombre) {
+//   console.log(`¡Hola, ${nombre}!`);
+// }
+
+// let nombreUsurio = prompt('Ingrese su nombre por favor!! : ');
+// saludo2(nombreUsurio);
+
+// DEV:TODO -> 3. Crear una función que reciba un número como parámetro y devuelva el doble de ese número.
+// function calculo(numeroCalculo) {
+//   return numeroCalculo*numeroCalculo;
+// }
+
+// let numeroCalcular = parseInt(prompt('Ingrese un número por favor!!'));
+// let calcular = calculo(numeroCalcular);
+// console.log(`El doble del numero ingresado es = ${calcular}`);
---
+++ sections/section02_funciones.js
@@ -0,0 +1,33 @@
+
+
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Juego del número secreto';
+
+// let parrafo = document.querySelector('p');
+// parrafo.innerHTML = 'Indica un número del 1 al 10';
+
+let numeroSecreto = generarNumeroScreto();
+
+function asignarTextoElemento(elemento, texto) {
+  // let titulo = document.querySelector('h1');
+  let elementoHTML = document.querySelector(elemento);
+  // titulo.innerHTML = 'Juego del número secreto';
+  elementoHTML.innerHTML = texto;
+  return;
+}
+
+function verificarIntento(){
+  // alert('Click desde el botón');
+  let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+  console.log(typeof(numeroDeUsuario));
+  console.log(numeroDeUsuario === numeroSecreto);
+  return;
+}
+
+function generarNumeroScreto() {
+  return Math.floor(Math.random()*10)+1;
+  // return numeroSecreto;
+}
+
+asignarTextoElemento('h1','Juego del número secreto');
+asignarTextoElemento('p','Indica un número del 1 al 100');
