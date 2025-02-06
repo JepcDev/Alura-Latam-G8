53a3c87ca69cea19283d4992673c7ead3df9abb9
JepcDev <joseenrique.pc21@gmail.com>
2025-02-05 21:11:57 -0400

    feat: add generar numero secreto alatorio

--- app.js
+++ app.js
@@ -6,15 +6,24 @@
 // let parrafo = document.querySelector('p');
 // parrafo.innerHTML = 'Indica un número del 1 al 10';
 
+let numeroSecreto = generarNumeroScreto();
+
 function asignarTextoElemento(elemento, texto) {
   // let titulo = document.querySelector('h1');
   let elementoHTML = document.querySelector(elemento);
   // titulo.innerHTML = 'Juego del número secreto';
   elementoHTML.innerHTML = texto;
+  return;
 }
 
 function intentoUsuario(){
   alert('Click desde el botón');
+  return;
+}
+
+function generarNumeroScreto() {
+  return Math.floor(Math.random()*10)+1;
+  // return numeroSecreto;
 }
 
 asignarTextoElemento('h1','Juego del número secreto');
